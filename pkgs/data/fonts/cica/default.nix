89e803d819bd037d2f0e0c09e51f42dab8330529
heyyou <hey3you3@gmail.com>
2021-09-07 00:58:26 +0900

    fix: Cica font zip sha256

--- pkgs/data/fonts/cica/default.nix
+++ pkgs/data/fonts/cica/default.nix
@@ -12,7 +12,7 @@ in fetchzip {
     unzip -j $downloadedFile \*.ttf -d $out/share/fonts/cica
   '';
 
-  sha256 = "0a1vp6v18yj5lspxk4n7l1r6ipabmpdwgjflg9alhs3gdwbg952c";
+  sha256 = "0zh1n5x1swgh72pyzgkvajs9r4xn172v6shl7978gjbh9xlqb8a2";
 
   meta = with lib; {
     description = "Programming Font";
